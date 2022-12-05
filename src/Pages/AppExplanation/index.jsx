10236dfad1c4b5dc360d0c52fa37d434cc337261
Luciano <lucianoalbertop@hotmail.com>
2022-12-05 00:23:26 -0300

    Edição e criação dos hábitos no app

--- src/Pages/AppExplanation/index.jsx
+++ src/Pages/AppExplanation/index.jsx
@@ -1,68 +1,67 @@
-import React from "react";
-import {ScrollView, View, StyleSheet, Text, handleNavHome} from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, ScrollView, handleSetShowHome } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
-import DefaultButton from "../../Components/Common/DefaultButton";
 import ExplanationCard from "../../Components/Explanation/ExplanationCard";
-
+import DefaultButton from "../../Components/Common/DefaultButton";
 
 export default function AppExplanation() {
 
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    function handleNavHome() {
-        navigation.navigate("Home");
-    }
+  function handleNavHome() {
+    navigation.navigate("Home");
+  }
 
-    return(
-        <View style={styles.container}>
-            <ScrollView>
-                <View style={{alignItems:"center"}}>
-                    <Text style={styles.title}>
-                        Antes, deixa {"\n"} eu te explicar...
-                    </Text>
-                    <ExplanationCard />
-                    <Text style={styles.descriptionCta}>
-                        Pronto(a) para subir de nível na vida?
-                    </Text>
-                    <Text style={styles.description}>
-                        Na próxima tela vocÊ vai poder escolher{"\n"} seus 4 hábitos de forma invidivual.
-                    </Text>
-                    <DefaultButton
-                        buttonText={"Continuar"}
-                        handlePress={handleNavHome}
-                        width={250}
-                        height={50}
-                    >
-                    </DefaultButton>
-                </View>
-            </ScrollView>
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <View style={{ alignItems: "center" }}>
+          <Text style={styles.title}>
+            Antes, deixa {"\n"} eu te explicar...
+          </Text>
+          <ExplanationCard />
+          <Text style={styles.descriptionCta}>
+            Pronto(a) para subir de nível na vida?
+          </Text>
+          <Text style={styles.description}>
+            Na próxima tela você vai poder escolher {"\n"} seus 4 hábitos de
+            forma individual.
+          </Text>
+         <DefaultButton
+            buttonText={"Continuar"}
+            handlePress={handleNavHome}
+            width={250}
+            height={50}
+          />
         </View>
-    );
+      </ScrollView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:"rgba(21,21,21, 0.98)",
-    },
-    title:{
-        fontSize:30,
-        fontWeight:"bold",
-        color:"white",
-        textAlign:"center",
-        marginVertical:40,
-    },
-    descriptionCta:{
-        color:"white",
-        fontSize:16,
-        fontWeight:"bold",
-        marginTop:20,
-        marginBottom:10,
-    },
-    description:{
-        color:"white",
-        marginBottom:30,
-        textAlign:"center",
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "rgba(21, 21, 21, 0.98)",
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "center",
+    marginVertical: 40,
+  },
+  descriptionCta: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 16,
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  description: {
+    color: "white",
+    textAlign: "center",
+    marginBottom: 30,
+  },
 });
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,60 +1,113 @@
-import React from "react";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import{View, Text, StyleSheet, ScrollView} from "react-native";
-
 
 import LifeStatus from "../../Components/Common/LifeStatus";
 import StatusBar from "../../Components/Home/StatusBar";
+import EditHabit from "../../Components/Home/EditHabit";
 import CreateHabit from "../../Components/Home/CreateHabit";
 
 
-export default function Home(){
-    const navigation = useNavigation();
 
-    function handleNavExplanation(){
-        navigation.navigate("AppExplanation");
-    }
+export default function Home() {
+	const navigation = useNavigation();
+  const [mindHabit, setMindHabit] = useState();
+  const [moneyHabit, setMoneyHabit] = useState();
+  const [bodyHabit, setBodyHabit] = useState();
+  const [funHabit, setFunHabit] = useState();
+    
+   
+function handleNavExplanation() {
+    navigation.navigate("AppExplanation");
+      }
+    
+return (
+    <View style={styles.container}>
+        <ScrollView>
+        <View style={{ alignItems: "center" }}>
+        <Text style={styles.dailyChecks}>❤️ 20 dias - ✔️ 80 checks</Text>
+    
+        <LifeStatus />
+        <StatusBar />
+        
+        {mindHabit ? (
+            <EditHabit
+                habit={mindHabit?.habitName}
+                frequency={`${mindHabit?.habitTime} - ${mindHabit?.habitFrequency}`}
+                habitArea={mindHabit?.habitArea}
+                checkColor="#90B7F3"
+            />
+        ) : ( 
 
-        return(
-            <View style={styles.container}>
-                <ScrollView>
-                    <View style={{alignItems: "center"}}>
-                        <Text style={styles.dailyChecks}>❤️ 20 dias - ✔️ 80 checks</Text>
-                        <LifeStatus />
-                        <StatusBar />
-                        <CreateHabit habitArea="Mente" borderColor= "#90B7F3" />
-                    </View>
-                    <Text
-                        style={styles.explanationText}
-                        onPress={() => {handleNavExplanation();
-                        }}
-                    >
-                        Ver explicações novamente
-                    </Text>
-                </ScrollView>
+            <CreateHabit habitArea="Mente" borderColor="#90B7F3" />
+        )}
+        {moneyHabit ? (
+            <EditHabit
+                habit={moneyHabit?.habitName}
+                frequency={`${moneyHabit?.habitTime} - ${moneyHabit?.habitFrequency}`}
+                habitArea={moneyHabit?.habitArea}
+                checkColor="#85BB65"
+            />
+        ) : (
+            
+            <CreateHabit habitArea="Financeiro" borderColor="#85BB65" />
+        )}
+        {bodyHabit ? (
+            <EditHabit
+                habit={bodyHabit?.habitName}
+                frequency={`${bodyHabit?.habitTime} - ${bodyHabit?.habitFrequency}`}
+                habitArea={bodyHabit?.habitArea}
+                checkColor="#FF0044"
+            />
+        ) : (
+                
+                
+                <CreateHabit habitArea="Corpo" borderColor="#FF0044" />
+        )}
+        {funHabit ? (
+            <EditHabit
+                habit={funHabit?.habitName}
+                frequency={`${funHabit?.habitTime} - ${funHabit?.habitFrequency}`}
+                habitArea={funHabit?.habitArea}
+                checkColor="#FE7F23"
+            />
+        ) : (
+            
+            <CreateHabit habitArea="Humor" borderColor="#FE7F23" />
+        )}
             </View>
-        );
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex:1,
-        backgroundColor: "rgba(21,21,21, 0.98)",
-    },
-    dailyChecks:{
+    
+            <Text
+              style={styles.explanationText}
+              onPress={() => {
+                handleNavExplanation();
+              }}
+            >
+              Ver explicações novamente
+            </Text>
+          </ScrollView>
+        </View>
+      );
+    }
+    
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: "rgba(21, 21, 21, 0.98)",
+      },
+      dailyChecks: {
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center",
+        fontSize: 18,
+        marginTop: 40,
+      },
+      explanationText:{
         color:"white",
-        fontWeight:"bold",
         textAlign:"center",
+        marginTop:10,
+        marginBottom:10,
+        fontWeight:"bold",
         fontSize:18,
-        marginTop:40,
-    },
-    explanationText:{
-        color:"white",
-        fontSize:16,
-        fontWeight:"bold",
-        textAlign:"center",
-        paddingTop:15,
-        paddingBottom:25,
-    },
-
-})
+      },
+    });
